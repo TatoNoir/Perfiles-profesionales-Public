70abe90e8fad17c59c2319acab6075df1d05e7d1
Mariano <marianopainefil@gmail.com>
2025-10-30 15:11:40 -0300

    Modificaciones menú sidebar

--- perfiles-profesionales/src/app/pro-dashboard/layout/pro-layout.component.ts
+++ perfiles-profesionales/src/app/pro-dashboard/layout/pro-layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon, IonMenu, IonMenuButton, IonList, IonItem, IonLabel, IonMenuToggle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon, IonMenu, IonList, IonItem, IonLabel, IonMenuToggle, IonSplitPane, IonMenuButton } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { home, personCircle, star, chatbubbles, logOut } from 'ionicons/icons';
 import { AuthService } from '../../auth/services/auth.service';
@@ -12,7 +12,7 @@ import { AuthService } from '../../auth/services/auth.service';
   imports: [
     CommonModule, RouterLink, RouterOutlet,
     IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon,
-    IonMenu, IonMenuButton, IonList, IonItem, IonLabel, IonMenuToggle
+    IonMenu, IonList, IonItem, IonLabel, IonMenuToggle, IonSplitPane, IonMenuButton
   ],
   templateUrl: './pro-layout.component.html',
   styleUrl: './pro-layout.component.css'
