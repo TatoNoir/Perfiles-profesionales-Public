3bda0b3488c8df039b93bdf31942522de52b3c1e
Mariano <marianopainefil@gmail.com>
2025-10-30 14:52:41 -0300

    Ajustes estilos mobile

--- perfiles-profesionales/src/app/pro-dashboard/layout/pro-layout.component.ts
+++ perfiles-profesionales/src/app/pro-dashboard/layout/pro-layout.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { IonContent, IonHeader, IonToolbar, IonTitle, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon, IonMenu, IonMenuButton, IonList, IonItem, IonLabel, IonMenuToggle } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { home, personCircle, star, chatbubbles, logOut } from 'ionicons/icons';
 import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-pro-layout',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, IonContent, IonHeader, IonToolbar, IonTitle, IonButton],
+  imports: [
+    CommonModule, RouterLink, RouterOutlet,
+    IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon,
+    IonMenu, IonMenuButton, IonList, IonItem, IonLabel, IonMenuToggle
+  ],
   templateUrl: './pro-layout.component.html',
   styleUrl: './pro-layout.component.css'
 })
 export class ProLayoutComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService) {
+    addIcons({ home, personCircle, star, chatbubbles, logOut });
+  }
   logout() { this.auth.logout(); }
 }
 
--- perfiles-profesionales/src/app/pro-dashboard/pages/overview/overview.component.ts
+++ perfiles-profesionales/src/app/pro-dashboard/pages/overview/overview.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonGrid, IonRow, IonCol, IonCard, IonCardContent, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { star, chatbubbles, personCircle, trendingUp } from 'ionicons/icons';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonGrid, IonRow, IonCol, IonCard, IonCardContent, IonIcon],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
-export class OverviewComponent {}
+export class OverviewComponent {
+  stats = {
+    averageRating: 4.6,
+    totalReviews: 23,
+    pendingQuestions: 5,
+    profileCompletion: 80
+  };
+
+  constructor() {
+    addIcons({ star, chatbubbles, personCircle, trendingUp });
+  }
+}
 
 
